8730461fb905d42e775a6d7e1d3d1bb03da6b268
indraarianggi <indraarianggi@gmail.com>
2019-07-25 22:02:51 +0700

    [update] gatsby init

---
+++ src/components/layout.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Footer from "./footer"
+
+import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
+
+const Layout = props => {
+  return (
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>{props.children}</div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/pages/credits.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Credit = () => {
+  return (
+    <div>
+      <h1>Credit Page</h1>
+    </div>
+  )
+}
+
+export default Credit

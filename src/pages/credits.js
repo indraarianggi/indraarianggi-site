b98e55980bda510c22fd76b82a661e67d688e13c
indraarianggi <indraarianggi@gmail.com>
2019-08-18 20:27:17 +0700

    [Update] credits page content

--- src/pages/credits.js
+++ src/pages/credits.js
@@ -5,12 +5,37 @@ import React from "react"
 import Layout from "../components/Layout/Layout"
 import Title from "../components/Title/Title"
 import Line from "../components/Line/Line"
+import TextBody from "../components/TextBody/TextBody"
 
 const Credit = () => {
   return (
     <Layout>
       <Title text="Credits" />
       <Line primary bold />
+      <TextBody>
+        <p>This website is inspired by awesome works from:</p>
+        <ul>
+          <li>
+            <a href="https://juliette.sh/">Juliette Pretot</a>
+          </li>
+          <li>
+            <a href="https://brittanychiang.com/">Brittany Chiang</a>
+          </li>
+          <li>
+            <a href="https://overreacted.io/">Dan Abramov</a>
+          </li>
+          <li>
+            <a href="https://kelimuttu.co/">Rizki D Kelimutu</a>
+          </li>
+        </ul>
+        <p>
+          Greate thanks for <a href="https://dribbble.com/">dribbble</a>, which
+          provide unlimited inspirations.
+          <br />
+          And <a href="https://www.flaticon.com/">FLATICON</a> for pretty icons
+          I use here.
+        </p>
+      </TextBody>
     </Layout>
   )
 }
